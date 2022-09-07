670ad4bb5ad802c6025752af272308272d847e28
Lars Nysom <LANY@ucn.dk>
2022-09-07 12:07:20 +0200

    Add project files.

---
+++ Calculator/SimpleCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace DebugAndTestDemo
+{
+    public class SimpleCalculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+    }
+}
---
+++ CalculatorTests/SimpleCalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using DebugAndTestDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugAndTestDemo.Tests
+{
+    public class SimpleCalculatorTests
+    {
+        [Fact()]
+        public void AddSuccessTest()
+        {
+            // Arrange
+            double a = 2;
+            double b = 2;
+            double expectedResult = 4;
+
+            // Act
+            double result = SimpleCalculator.Add(a, b);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+
+        }
+
+        [Fact()]
+        public void AddAnotherSuccessTest()
+        {
+            // Arrange
+            double a = 2;
+            double b = 3;
+            double expectedResult = 5;
+
+            // Act
+            double result = SimpleCalculator.Add(a, b);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+
+        }
+    }
+}
